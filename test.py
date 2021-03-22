b25005c9c7d9b824d84e3456ce6a0409cfc78e48
Vovchek <Vlad.aks03@mail.ru>
2021-03-22 23:04:19 +0500

    улучшил тест

--- test.py
+++ test.py
@@ -9,4 +9,6 @@ j += [requests.get('http://127.0.0.1:5000/api/get_job/1').json()]
 j += [requests.get('http://127.0.0.1:5000/api/get_job/3').json()]
 j += [requests.get('http://127.0.0.1:5000/api/get_job/q').json()]
 j += [requests.delete('http://127.0.0.1:5000/api/del_job/3').json()]
+j += [requests.delete('http://127.0.0.1:5000/api/del_job/3').json()]
+j += [requests.delete('http://127.0.0.1:5000/api/del_job/щроца').json()]
 pprint(j)
